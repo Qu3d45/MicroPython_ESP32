60c607fbc8d322f722df93fbbc85c228a34ae63b
qu3d45 <qu3d45@gmail.com>
2020-01-21 20:06:09 +0000

    loboris_backup

--- esp32_0_Codigo_Dissertação/main_loboris_funciona.py
+++ esp32_0_Codigo_Dissertação/main_loboris_funciona.py
@@ -39,10 +39,70 @@
 
 from machine import Pin, I2C, time_pulse_us, Onewire, DHT, ADC
 from time import sleep, sleep_ms, sleep_us
+
 #from onewire import OneWire
 #from ds18x20 import DS18X20
 
 #import dht
+# Channel ID 902540
+# KEY IL9VIMCHEXM9H3W4
+# GET https://api.thingspeak.com/update?api_key=IL9VIMCHEXM9H3W4&field1=0
+
+import socket
+import machine
+import time
+import sys
+import gsm
+
+import json
+import urequests
+#import wlan
+
+
+# APN credentials (replace with yours)
+
+GSM_APN = 'Qu3d45_ESP32'  # Your APN
+GSM_USER = ''  # Your User
+GSM_PASS = ''  # Your Pass
+
+# Power on the GSM module
+
+GSM_PWR = machine.Pin(4, machine.Pin.OUT)
+GSM_RST = machine.Pin(5, machine.Pin.OUT)
+GSM_MODEM_PWR = machine.Pin(23, machine.Pin.OUT)
+
+GSM_PWR.value(0)
+GSM_RST.value(1)
+GSM_MODEM_PWR.value(1)
+
+# Init PPPoS
+
+# gsm.debug(True)  # Uncomment this to see more logs, investigate issues, etc.
+
+gsm.start(tx=27, rx=26, apn=GSM_APN, user=GSM_USER, password=GSM_PASS)
+
+sys.stdout.write('Waiting for AT command response...')
+for retry in range(20):
+    if gsm.atcmd('AT'):
+        break
+    else:
+        sys.stdout.write('.')
+        time.sleep_ms(5000)
+else:
+    raise Exception("Modem not responding!")
+print()
+
+print("Connecting to GSM...")
+gsm.connect()
+
+while gsm.status()[0] != 1:
+    pass
+
+print('IP:', gsm.ifconfig()[0])
+
+# GSM connection is complete.
+# You can now use modules like urequests, uPing, etc.
+
 
 def ReadDS18(ds18_sensor):
     
@@ -203,8 +263,11 @@ else:
     print(discharge, " m3/s")
 
 
+#####----- RTC -----#####
 
 
+#####----- Thingspeak -----#####
 
+url = "https://api.thingspeak.com/update?api_key=IL9VIMCHEXM9H3W4&field1={}&field2={}&field3={}&field4={}&field5={}&field6={}&field7={}".format(ds18_temp, pH_final, discharge, tdsValue, temp_dht22, hum_dht22, ecValue)
 
-
+update = urequests.get(url)
